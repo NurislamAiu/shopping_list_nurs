512bbf8f16ca7fcb0201dbe2869dbfb07060359d
nurislamilyasov <nurikileasov0000@gmail.com>
2025-03-16 14:59:29 +0500

    was created function on the creating new items

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,24 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list_nurs/data/dummy_item.dart';
+import 'package:shopping_list_nurs/models/grocery_item.dart';
+import 'package:shopping_list_nurs/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem() async {
+    final newItem = await Navigator.push<GroceryItem>(
+      context,
+      MaterialPageRoute(builder: (ctx) => NewItemScreen()),
+    );
+
+    if (newItem == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Your Groceries')),
+      appBar: AppBar(
+        title: Text('Your Groceries'),
+        actions: [IconButton(onPressed: _addItem, icon: Icon(Icons.add))],
+      ),
       body: ListView.builder(
-        itemCount: groceryItems.length,
+        itemCount: _groceryItems.length,
         itemBuilder:
             (ctx, index) => ListTile(
-              title: Text(groceryItems[index].name),
+              title: Text(_groceryItems[index].name),
               leading: Container(
                 width: 24,
                 height: 24,
-                color: groceryItems[index].category.color,
+                color: _groceryItems[index].category.color,
               ),
-              trailing: Text(groceryItems[index].quantity.toString()),
+              trailing: Text(_groceryItems[index].quantity.toString()),
             ),
       ),
     );
